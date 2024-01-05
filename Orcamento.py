9216fe5081432e16ab90ff8b17d3284930d67e10
Kaynam DevPython <kaysenpai2@gmail.com>
2024-01-05 17:56:53 -0300

    atualizaçoes

--- Orcamento.py
+++ Orcamento.py
@@ -1,18 +1,20 @@
-class _Funcionarios:
+from Entidades.Diaria import Diaria
+from Servicos.Servico_Funcao import Servico_Funcao
+
+class Funcionarios:
     def __init__(self, id, cpf, nome, funcao) -> object:
         self._id = id
         self._cpf = cpf
-        self._nome = nome
-        self._funcao = funcao 
+        self.nome = nome
+        self.funcao = funcao 
 
 class Veiculos:
-    def __init__(self, marca, placa, ano, autonomia, combustivel) -> object:
-        self._marca = marca
+    def __init__(self, placa, marca, ano, autonomia, combustivel) -> object:
         self._placa = placa
-        self._ano = ano 
-        self._autonomia = autonomia
-        self._combustivel = combustivel
-
+        self.marca = marca
+        self.ano = ano 
+        self.autonomia = autonomia
+        self.combustivel = combustivel
 
 class Equipamentos_Alugaveis:
     def __init__(self, id, func_associado, tipo_equipamento, horas_equipamento) -> object:
@@ -21,27 +23,18 @@ class Equipamentos_Alugaveis:
         self.tipo_equipamento = tipo_equipamento 
         self.horas_equipamento = horas_equipamento
 
+class Orcamento_combustivel:
+    def __init__(self, distancia, valor_combustivel) -> object:
+        self.distancia = distancia
+        self.valor_combustivel = valor_combustivel
 ## forma de chamar o funcionado da classe equipamentos alugaveis com id da class funcionario
 #print(.funcionario_associado._id)
-        
 
-funcoes_funcionarios = [
-    {
-        'id_funçoes': 1,
-        'funçao': 'Motorista de Retroescavadeira.'
-    },
-    {
-        'id_funçoes': 2,
-        'funçao': 'Motorista de Empilhadeira.'
-    },
-    {
-        'id_funçoes': 3,
-        'funçao': 'Instalador de Equipamentos.'
-    },
-    {
-        'id_funçoes':4,
-        'funçao': 'Técnico em Mecânica.'
-    }
-]
+Svc_Funcao = Servico_Funcao()
+Svc_Funcao.adicionar_funcao('Pedreiro')
+for funcao in Svc_Funcao.obter_funcoes():
+    print(f"ID: {funcao['id']}, Função: {funcao['funcao']}")
 
-
+diarias = Diaria(input=('')) 
+valor_diarias = diarias.calcular_diaria()
+print(valor_diarias)
---
+++ Servicos/Servico_Funcao.py
@@ -0,0 +1,39 @@
+import random
+
+class Servico_Funcao:
+    contador_id = 1  # Inicializa o contador de ID
+
+    def __init__(self):
+        self.lista_funcoes = [
+            {
+                'id': 1,
+                'funcao': 'Motorista de Retroescavadeira.'
+            },
+            {
+                'id': 2,
+                'funcao': 'Motorista de Empilhadeira.'
+            },
+            {
+                'id': 3,
+                'funcao': 'Instalador de Equipamentos.'
+            },
+            {
+                'id': 4,
+                'funcao': 'Técnico em Mecânica.'
+            }
+        ]
+
+    def obter_funcoes(self):
+        return self.lista_funcoes
+
+    def adicionar_funcao(self, nome_funcao):
+        if self.lista_funcoes:
+            ultimo_id = self.lista_funcoes[-1]['id']
+            novo_id = ultimo_id + 1
+        else:
+            novo_id = Servico_Funcao.contador_id
+
+        self.lista_funcoes.append({
+            'id': novo_id,
+            'funcao': nome_funcao
+        })
